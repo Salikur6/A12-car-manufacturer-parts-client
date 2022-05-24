097fe159ec03bb6357b25b75d50be4be1e771690
salikur6 <salikur6@gmail.com>
2022-05-24 18:27:55 +0600

    partsitems section some changes and footer section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import Purchase from './components/Purchase/Purchase';
+import Footer from './components/Shared/Footer';
 import Navbar from './components/Shared/Navbar';
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Home/PartsItems.js
+++ src/components/Home/PartsItems.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const PartsItems = () => {
     const [parts, setParts] = useState([]);
@@ -10,21 +11,26 @@ const PartsItems = () => {
             .then(data => setParts(data))
     }, [])
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto my-20'>
+
+            <div className='my-16'>
+                <h2 className='text-4xl font-bold text-center uppercase'>All of our products</h2>
+                <p></p>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center'>
-                {parts.map(part =>
-                    <div class="card glass" style={{ maxWidth: '320px' }}>
-                        <figure><img src={part?.img} style={{ borderRadius: '10px' }} className='mt-5' alt="car!" /></figure>
+                {parts.slice(0, 6).map(part =>
+                    <div class="card glass" style={{ maxWidth: '400px' }}>
+                        <figure><img src={part?.img} style={{ borderRadius: '10px', width: '270px' }} className='mt-5' alt="car!" /></figure>
                         <div class="card-body">
-                            <h2 class="card-title">{part?.name}</h2>
-                            <p>{part?.description}</p>
-                            <div>
-                                <p>Available Quantity: {part?.availableQuantity}</p>
-                                <p>Minimum Order Quantity: {part?.minimumQuantity}</p>
+                            <h2 class="card-title font-bold text-2xl" style={{ fontFamily: " 'Cinzel', serif" }}>{part?.name}</h2>
+                            <p className='text-justify'>{part?.description}</p>
+                            <div className='mt-3'>
+                                <p className='font-bold mb-1' style={{ fontFamily: " 'Cinzel', serif" }}>Available Quantity: {part?.availableQuantity}</p>
+                                <p className='font-bold' style={{ fontFamily: " 'Cinzel', serif" }}>Minimum Order Quantity: {part?.minimumQuantity}</p>
                             </div>
-                            <p className='font-bold'>Price: ${part?.price}</p>
+                            <p className='font-bold text-center my-3'>Price: ${part?.price}</p>
                             <div class="card-actions justify-center">
-                                <button class="btn btn-primary">Buy Now</button>
+                                <Link to='/purchase' class="btn btn-primary">Buy Now</Link>
                             </div>
                         </div>
                     </div>
